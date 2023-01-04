e4d8714c1eae9ea1aaaf66fd8fc2849225d668c0
tuanphamminh2 <tuan.phamminh2@ncc.asia>
2023-01-04 13:09:39 +0700

    animation button close header

--- js/header.js
+++ js/header.js
@@ -1,7 +1,6 @@
 const navCol = document.querySelector(".navbar-collapse");
 const btnMenu = document.querySelector(".menu-icon-btn");
-
-
+btnMenu.addEventListener( "click", () => btnMenu.classList.toggle("toggled") );
 
 btnMenu &&
   btnMenu.addEventListener("click", () => {
