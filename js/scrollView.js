be4dada2aafa1ce69c593dd39711e3932c7f16dc
tuanphamminh2 <tuan.phamminh2@ncc.asia>
2023-01-06 13:40:31 +0700

    fix aniamtion

--- js/scrollView.js
+++ js/scrollView.js
@@ -1,3 +1,6 @@
+let valueDisplays = document.querySelectorAll(".num-count");
+let interval = 900;
+
 function scrollTrigger(selector, options = {}) {
   let els = document.querySelectorAll(selector);
   els = Array.from(els);
@@ -21,6 +24,20 @@ function addObserver(el, options) {
           options.cb(el);
         } else {
           entry.target.classList.add("active");
+          valueDisplays.forEach((valueDisplay, index) => {
+            setTimeout(function () {
+              let startValue = 0;
+              let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+              let duration = Math.floor(interval / endValue);
+              let counter = setInterval(function () {
+                startValue += 1;
+                valueDisplay.textContent = startValue;
+                if (startValue == endValue) {
+                  clearInterval(counter);
+                }
+              }, duration);
+            }, index * 700);
+          });
         }
         observer.unobserve(entry.target);
       }
@@ -30,23 +47,14 @@ function addObserver(el, options) {
 }
 scrollTrigger(".scrollBox", {
   rootMargin: "00px",
+  
 });
 
+scrollTrigger(".scrollBox2", {
+    rootMargin: "00px",
+    
+  });
 //animation number cout
-let valueDisplays = document.querySelectorAll(".num-count");
-let interval = 900;
 
-valueDisplays.forEach((valueDisplay, index) => {
-  setTimeout(function () {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function () {
-      startValue += 1;
-      valueDisplay.textContent = startValue;
-      if (startValue == endValue) {
-        clearInterval(counter);
-      }
-    }, duration);
-  }, index * 750);
-});
+
+
