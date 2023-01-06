fddf363d100166a803310233dfa296f273747b52
tuanphamminh2 <tuan.phamminh2@ncc.asia>
2023-01-06 13:26:05 +0700

    add animation ScrollView

---
+++ js/scrollView.js
@@ -0,0 +1,53 @@
+function scrollTrigger(selector, options = {}) {
+  let els = document.querySelectorAll(selector);
+  els = Array.from(els);
+  els.forEach((el) => {
+    addObserver(el, options);
+  });
+}
+function addObserver(el, options) {
+  if (!("IntersectionObserver" in window)) {
+    if (options.cb) {
+      options.cb(el);
+    } else {
+      entry.target.classList.add("active");
+    }
+    return;
+  }
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (options.cb) {
+          options.cb(el);
+        } else {
+          entry.target.classList.add("active");
+        }
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+  observer.observe(el);
+}
+scrollTrigger(".scrollBox", {
+  rootMargin: "00px",
+});
+
+//animation number cout
+let valueDisplays = document.querySelectorAll(".num-count");
+let interval = 900;
+
+valueDisplays.forEach((valueDisplay, index) => {
+  setTimeout(function () {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    console.log(duration, endValue);
+    let counter = setInterval(function () {
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if (startValue == endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
+  }, index * 750);
+});
