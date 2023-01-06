7aa1377433e12b7c98a7bec018af3c344e48dbe2
hai.nguyensinh <duykhanhslna@gmail.com>
2023-01-06 18:14:41 +0700

    update ui responsive

--- js/scrollView.js
+++ js/scrollView.js
@@ -24,22 +24,22 @@ function addObserver(el, options) {
           options.cb(el);
         } else {
           entry.target.classList.add("active");
-         if(el.classList.contains('scrollBox2')){
-
-          valueDisplays.forEach((valueDisplay, index) => {
-            setTimeout(function () {
-              let startValue = 0;
-              let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-              let duration = Math.floor(interval / endValue);
-              let counter = setInterval(function () {
-                startValue += 1;
-                valueDisplay.textContent = startValue;
-                if (startValue == endValue) {
-                  clearInterval(counter);
-                }
-              }, duration);
-            }, index * 600);
-          });}
+          if (el.classList.contains("scrollBox2")) {
+            valueDisplays.forEach((valueDisplay, index) => {
+              setTimeout(function () {
+                let startValue = 0;
+                let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+                let duration = Math.floor(interval / endValue);
+                let counter = setInterval(function () {
+                  startValue += 1;
+                  valueDisplay.textContent = startValue;
+                  if (startValue == endValue) {
+                    clearInterval(counter);
+                  }
+                }, duration);
+              }, index * 600);
+            });
+          }
         }
         observer.unobserve(entry.target);
       }
@@ -49,14 +49,51 @@ function addObserver(el, options) {
 }
 scrollTrigger(".scrollBox", {
   rootMargin: "00px",
-  
 });
 
 scrollTrigger(".scrollBox2", {
-    rootMargin: "00px",
-    
-  });
+  rootMargin: "00px",
+});
 //animation number cout
+let width 
+let height
 
+const changeCoords = (areaID) => {
+  let newCoords;
+  const radiusImg = 100 / 750;
+  if (areaID == "trannhan") {
+    const imgTopX = 393 / 750;
+    const imgTopY = 330 / 1000;
+    newCoords = `${imgTopX * width }, ${imgTopY * height }, ${radiusImg * width  }`;
+  } else if (areaID == "lelinh") {
+    const imgLeftX = 146 / 750;
+    const imgLeftY = 405 / 1000;
+    newCoords = `${imgLeftX * width }, ${imgLeftY * height}, ${radiusImg * width }`;
+  } else if (areaID == "tungnguyen") {
+    const imgRightX = 324 / 750;
+    const imgRightY = 556 / 1000;
+    newCoords = `${imgRightX * width}, ${imgRightY * height}, ${radiusImg * width }`;
+  } else {
+    const imgBottomX = 576 / 750;
+    const imgBottomY = 473 / 1000;
+    newCoords = `${imgBottomX * width }, ${imgBottomY * height }, ${radiusImg * width }`;
+  }
+  const x = document.getElementById(areaID);
+  x.setAttribute("coords", newCoords);
+};
 
+function setCoordIntoAreaElement () {
+  const mapboss = document.getElementById("mapboss")
+  Array.from(mapboss.children).forEach(el => changeCoords(el.id))
+}
+
+function setSizeBrower () {
+  let img = document.querySelector('.our-image img');
+  width = img.clientWidth;
+  height = img.clientHeight;
+}
+
+setSizeBrower()
+window.addEventListener('resize', () => {setSizeBrower() ; setCoordIntoAreaElement()})
+setCoordIntoAreaElement()
 
