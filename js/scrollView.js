6bdb3dc283fe03d0ac37a1e078433f0a0f902631
tuanphamminh2 <tuan.phamminh2@ncc.asia>
2023-01-06 13:28:35 +0700

    remove console.log

--- js/scrollView.js
+++ js/scrollView.js
@@ -41,7 +41,6 @@ valueDisplays.forEach((valueDisplay, index) => {
     let startValue = 0;
     let endValue = parseInt(valueDisplay.getAttribute("data-val"));
     let duration = Math.floor(interval / endValue);
-    console.log(duration, endValue);
     let counter = setInterval(function () {
       startValue += 1;
       valueDisplay.textContent = startValue;
