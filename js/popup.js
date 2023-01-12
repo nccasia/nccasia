c52afd1e267cfee5254d0b6ab2284b98e0ae4896
tuanphamminh2 <tuan.phamminh2@ncc.asia>
2023-01-12 09:53:48 +0700

    add api for mobile screen

--- js/popup.js
+++ js/popup.js
@@ -6,6 +6,34 @@ const popup = document.querySelector(".modal");
 const popupMobile = document.querySelector(".mobile-modal");
 const btnSendPopup = document.querySelector(".modal_popup_main button");
 const inputPopup = document.querySelector(".modal-input");
+const inputPopupMobile = document.querySelector(".modal-input-mb");
+
+const btnCheck = document.getElementsByClassName("cbPopup");
+//Function
+const handleClosePopUp = () => {
+  popup.style.display = "none";
+  popup.classList.remove("hidePopup");
+};
+const handleClosePopUpMobile = () => {
+  popupMobile.style.display = "none";
+  popupMobile.classList.remove("hidePopup");
+};
+const handleOpenPopUp = () => {
+  popup.style.display = "block";
+};
+const handleOpenPopUpMobile = () => {
+  popupMobile.style.display = "block";
+};
+const resetPopUp = () => {
+  category = [];
+  inputPopup.value = "";
+};
+const resetPopUpMobile = () => {
+  category = [];
+  inputPopupMobile.value = "";
+};
+// Function end
+
 // Click Send btn
 btnSendPopup.addEventListener("click", function () {
   popup.classList.add("hidePopup");
@@ -14,49 +42,73 @@ btnCancelPopupMobile.addEventListener("click", function () {
   popupMobile.classList.add("hidePopup");
 });
 // Click outside Popup
-let category = 0;
+let category = [];
 popup.addEventListener("click", function (e) {
   if (e.target === e.currentTarget) {
     popup.classList.add("hidePopup");
-    inputPopup.value = "";
+    resetPopUp();
   }
 });
 
 //Event Click  show popup
-function handleTogglePopup() {
-  popup.style.display = "block";
-}
 for (var i = 0; i < btnPopup.length; i++) {
   btnPopup[i].addEventListener("click", function (e) {
-    handleTogglePopup();
-    category = e.target.dataset.id;
+    handleOpenPopUp();
+    resetPopUp();
+    category.push(Number(e.target.dataset.id));
   });
 }
+//Event Click  show popup mobile
 btnPopupMobile.addEventListener("click", function () {
-  popupMobile.style.display = "block";
-
+  handleOpenPopUpMobile();
+  resetPopUpMobile();
+  for (var i = 0; btnCheck[i]; ++i) {
+    btnCheck[i].checked = false;
+  }
 });
 //event add animation when close
 popup.addEventListener("animationend", function () {
   if (this.classList.contains("hidePopup")) {
-    this.style.display = "none";
-    this.classList.remove("hidePopup");
+    handleClosePopUp();
+    resetPopUp();
   }
 });
+//event add animation when close Popup mobile
 popupMobile.addEventListener("animationend", function () {
   if (this.classList.contains("hidePopup")) {
-    this.style.display = "none";
-    this.classList.remove("hidePopup");
+    handleClosePopUpMobile();
+    resetPopUpMobile();
   }
 });
+// sendApi for PC screen
 const formSendEmail = document.querySelector(".send-email");
 formSendEmail.addEventListener("click", (event) => {
   event.preventDefault();
   const email = document.getElementById("email-project").value;
   if (email === "") {
     return printMess("emailError", "Please fill out all required fields.");
+  } else {
+    handleClosePopUp();
+    sendApi(email);
   }
-
+});
+// sendApi for Mobile screen
+const formSendEmailMobile = document.querySelector(".send-email-mb");
+formSendEmailMobile.addEventListener("click", (event) => {
+  const emailMobile = document.getElementById("email-project-mb").value;
+  if (emailMobile === "") {
+    return printMess("emailError", "Please fill out all required fields.");
+  } else {
+    for (var i = 0; btnCheck[i]; ++i) {
+      if (btnCheck[i].checked) {
+        category.push(Number(btnCheck[i].value));
+      }
+    }
+    handleClosePopUpMobile();
+    sendApi(emailMobile);
+  }
+});
+function sendApi(email) {
   fetch("http://localhost:8800/ncc-site-api-sendmail", {
     method: "POST",
     headers: {
@@ -65,7 +117,7 @@ formSendEmail.addEventListener("click", (event) => {
     body: JSON.stringify({
       email: email,
       type: "case_study",
-      category:category,
+      category: category,
     }),
   })
     .then((res) => res.json())
@@ -86,4 +138,4 @@ formSendEmail.addEventListener("click", (event) => {
         "Oops, something went wrong. Please try again later."
       );
     });
-});
+}
