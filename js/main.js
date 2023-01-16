6242f52460049939fd05deb6891a8df6309bd987
hai.nguyensinh <duykhanhslna@gmail.com>
2023-01-16 11:38:26 +0700

    fix js

--- js/main.js
+++ js/main.js
@@ -484,7 +484,7 @@ const validateEmailSur = (email) => {
 
 const validatePhoneSurvey = (phoneSurvey) => {
   return phoneSurvey.match(
-    /(^((\+\d{1,2}|1)[\s.-]?)?\(?[2-9](?!11)\d{2}\)?[\s.-]?\d{3}[\s.-]?\d{4}$|^$)/
+    /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
   );
 };
 
